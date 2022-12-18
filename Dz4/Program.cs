511285274f6c5255cba4997f4518e3240f0ca47f
Kris <kristina.protasova.1992@mail.ru>
2022-12-18 17:16:33 +0300

    Dz4

---
+++ Dz4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Выяснить является ли число чётным
+Console.Write("Введите число: ");
+string? inputline = Console.ReadLine();
+
+//проверка на пустое значение
+if (inputline != null)
+{
+    int inputNumber = int.Parse(inputline);// присваиваем вещественное значение
+    int outResult = inputNumber % 2;// проверяем остаток от деления на 2
+    if (outResult == 0)
+    {
+       Console.WriteLine("Итог: Четное") ;// выводим если остатка нет
+    }
+    else
+    {
+        Console.WriteLine("Итог: Нечетное");// выводим если остаток есть
+    }
+}
