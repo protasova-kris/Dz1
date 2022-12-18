3328b38c6ce8bf0a4c4646e5eb92188c1ec1693e
Kris <kristina.protasova.1992@mail.ru>
2022-12-18 16:50:11 +0300

    Dz3

---
+++ Dz3/Program.cs
@@ -0,0 +1,46 @@
+﻿//  3. Найти максимальное из трех чисел
+
+
+//считываем данные
+Console.Write("Введите первое число: ");
+string? firstLine = Console.ReadLine();
+Console.Write("Введите второе число: ");
+string? secondLine = Console.ReadLine();
+Console.Write("Введите третье число: ");
+string? thirdLine = Console.ReadLine();
+
+//проверка на пустое значение
+
+if (firstLine != null && secondLine != null && thirdLine != null)
+{
+    // присваиваем вещественное значение
+
+    int firstNum = int.Parse(firstLine);
+    int secondNum = int.Parse(secondLine);
+    int thirdNum = int.Parse(thirdLine);
+    //сравниваем числа и выводим результат
+    if (firstNum > secondNum)
+    {
+        if (firstNum > thirdNum)
+        {
+            Console.WriteLine("Первое число максимальное");
+        }
+        else
+        {
+            Console.WriteLine("Третье число максимальное");
+        }
+
+    }
+    else
+    {
+        if (secondNum > thirdNum)
+        {
+            Console.WriteLine("Второе число максимальное");
+        }
+        else
+        {
+            Console.WriteLine("Третье число максимальное");
+        }
+
+    }
+}
