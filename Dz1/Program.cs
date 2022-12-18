f3b70a04e3560b014f9812d984bbb3fa88282a2c
Kris <kristina.protasova.1992@mail.ru>
2022-12-18 16:37:10 +0300

    DZ1

--- Dz1/Program.cs
+++ Dz1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Укажите первое число ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Укажите второе число ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Узнаем является ли первое число квадратом второго");
+if (b*b==a){
+    Console.WriteLine("Первое число является квадратом второго.");
+}
+else {
+    Console.WriteLine("Первое число не является квадратом второго.");
+}
