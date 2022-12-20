5deee567716ab4b6e80f5d6f5ac8aed946a3970f
Kris <kristina.protasova.1992@mail.ru>
2022-12-20 10:51:27 +0300

    Dz11

---
+++ Dz11/Program.cs
@@ -0,0 +1,17 @@
+﻿// 11. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+//проверка на пустое значение
+if (inputLine != null)
+{   //проверка длины массива
+    if (inputLine.Length >= 3)
+    {   //Преобразование строки в массив
+        char[] charArray = inputLine.ToString().ToCharArray();
+        //вывод данных
+        Console.WriteLine(charArray[2]);
+    }
+    else
+    {   //вывод в случае не соответствия длине массива
+        Console.Write("число слишком короткое");
+    }
+}
