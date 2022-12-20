5675b2dc21aa54031933be7bf3eb97b4c2c31606
Kris <kristina.protasova.1992@mail.ru>
2022-12-20 11:15:14 +0300

    Dz12

---
+++ Dz12/Program.cs
@@ -0,0 +1,26 @@
+﻿// 12.Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+
+//Считываем данные
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine() ?? "";
+// присваиваем вещественное значение
+int inputNumber = int.Parse(inputLine);
+if (inputNumber > 7 || inputNumber < 1)
+{
+    Console.WriteLine("Неделя - 7 дней, следовательно: такого дня нет");
+}
+else
+{
+    if (inputNumber == 6 || inputNumber == 7)
+    {
+        //вывод данных
+        Console.WriteLine("Выходной день");
+    }
+    else
+    {
+        //вывод при невыполнении условия 
+        Console.WriteLine("Будний день");
+    }
+}
