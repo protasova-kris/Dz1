acd78e3b6387b962a7b95d2f720e8daacfa6db2f
Kris <kristina.protasova.1992@mail.ru>
2022-12-24 14:45:06 +0300

    Dz25.2

---
+++ Dz25/Program.cs
@@ -0,0 +1,18 @@
+﻿// 25.Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+
+
+Console.WriteLine("Введите число А и число В");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+int index = 1;
+int result = 1;
+while (index <= B)
+{
+    result = result * A;
+    index = index + 1;
+}
+Console.WriteLine(result);
