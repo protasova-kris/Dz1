7a68902aff1eb9dcf8cd92e5f0e9450d63e54b9c
Kris <kristina.protasova.1992@mail.ru>
2022-12-29 17:52:50 +0300

    Dz47/1

---
+++ Dz47/Program.cs
@@ -0,0 +1,28 @@
+﻿//  47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами от -10,0 до 10,0.
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) / 10;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array [i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] numbers  = new double [3, 4];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+PrintArray(numbers);
